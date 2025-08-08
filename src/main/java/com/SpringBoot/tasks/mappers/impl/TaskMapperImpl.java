cd509e1aaaaeef82b50eee17ba40df91f73b8f5d
BuildTools <melihselimog_2007@hotmail.com>
2025-08-08 22:48:12 +0300

    TaskMapperImpl

---
+++ src/main/java/com/SpringBoot/tasks/mappers/impl/TaskMapperImpl.java
@@ -0,0 +1,39 @@
+package com.SpringBoot.tasks.mappers.impl;
+
+import org.springframework.stereotype.Component;
+
+import com.SpringBoot.tasks.domain.dto.TaskDto;
+import com.SpringBoot.tasks.domain.entities.Task;
+import com.SpringBoot.tasks.mappers.TaskMapper;
+
+@Component
+public class TaskMapperImpl implements TaskMapper {
+
+    @Override
+    public Task fromDto(TaskDto taskDto) {
+        return new Task(
+            taskDto.id(),
+            taskDto.title(),
+            taskDto.description(),
+            taskDto.priority(),
+            taskDto.status(),
+            taskDto.dueDate(),
+            null,
+            null,
+            null 
+        );
+    }
+
+    @Override
+    public TaskDto toDto(Task task) {
+       return new TaskDto(
+            task.getId(),
+            task.getTitle(),
+            task.getDescription(),
+            task.getDueDate(),
+            task.getPriority(),
+            task.getStatus()
+        );
+    }
+    
+}
