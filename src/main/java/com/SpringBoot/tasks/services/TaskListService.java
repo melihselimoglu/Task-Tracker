1d01830c9d78f565631e0510b7be1526bb7848b9
BuildTools <melihselimog_2007@hotmail.com>
2025-08-09 20:28:04 +0300

    TaskListService

---
+++ src/main/java/com/SpringBoot/tasks/services/TaskListService.java
@@ -0,0 +1,9 @@
+package com.SpringBoot.tasks.services;
+
+import java.util.List;
+
+import com.SpringBoot.tasks.domain.entities.TaskList;
+
+public interface TaskListService {
+    List<TaskList> listTaskLists();
+}
