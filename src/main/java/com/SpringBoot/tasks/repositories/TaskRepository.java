fb5c3d64a599cb001c008667e029a65a199c7604
BuildTools <melihselimog_2007@hotmail.com>
2025-08-09 20:26:01 +0300

    TaskRepository

---
+++ src/main/java/com/SpringBoot/tasks/repositories/TaskRepository.java
@@ -0,0 +1,17 @@
+package com.SpringBoot.tasks.repositories;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.SpringBoot.tasks.domain.entities.Task;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task, UUID> {
+    List<Task> findByTaskListId(UUID taskListId);
+    Optional<Task> findByTaskListIdAndId(UUID taskListid, UUID Id);
+    
+}
