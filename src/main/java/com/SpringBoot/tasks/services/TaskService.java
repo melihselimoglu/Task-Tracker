a679209f9079345af0627df22983602e02095308
BuildTools <melihselimog_2007@hotmail.com>
2025-08-13 21:47:30 +0300

    getTask method added

--- src/main/java/com/SpringBoot/tasks/services/TaskService.java
+++ src/main/java/com/SpringBoot/tasks/services/TaskService.java
@@ -1,5 +1,6 @@
 package com.SpringBoot.tasks.services;
 
+import java.util.Optional;
 import java.util.List;
 import java.util.UUID;
 
@@ -8,4 +9,5 @@ import com.SpringBoot.tasks.domain.entities.Task;
 public interface TaskService {
     List<Task> listTasks(UUID taskListId);
     Task createTask(UUID taskListId, Task task);
+    Optional<Task> getTask(UUID taskListId, UUID taskId);
 }
