5bbdb0ad1e9b8b8ea10bdd98c692157309de9093
BuildTools <melihselimog_2007@hotmail.com>
2025-08-13 21:35:21 +0300

    Tasks creation added to Impl

--- src/main/java/com/SpringBoot/tasks/services/impl/TaskServiceImpl.java
+++ src/main/java/com/SpringBoot/tasks/services/impl/TaskServiceImpl.java
@@ -1,11 +1,18 @@
 package com.SpringBoot.tasks.services.impl;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
+import org.springframework.cglib.core.Local;
 import org.springframework.stereotype.Service;
 
 import com.SpringBoot.tasks.domain.entities.Task;
+import com.SpringBoot.tasks.domain.entities.TaskList;
+import com.SpringBoot.tasks.domain.entities.TaskPriority;
+import com.SpringBoot.tasks.domain.entities.TaskStatus;
+import com.SpringBoot.tasks.repositories.TaskListRepository;
 import com.SpringBoot.tasks.repositories.TaskRepository;
 import com.SpringBoot.tasks.services.TaskService;
 
@@ -13,15 +20,50 @@ import com.SpringBoot.tasks.services.TaskService;
 public class TaskServiceImpl implements TaskService {
 
     private final TaskRepository taskRepository;
+    private final TaskListRepository taskListRepository;
 
     public TaskServiceImpl(TaskRepository taskRepository) {
         this.taskRepository = taskRepository;
+        this.taskListRepository = null;
     }
 
     @Override
     public List<Task> listTasks(UUID taskListId) {
         
         return taskRepository.findByTaskListId(taskListId); 
+    }
+
+    @Override
+    public Task createTask(UUID taskListId, Task task) {
+        if(null != task.getId()){
+            throw new IllegalArgumentException("Task already has an ID");
+        }
+        if (null == task.getTitle() || task.getTitle().isBlank()) {
+            throw new IllegalArgumentException("Task title cannot be null or blank");
+        }
+
+        TaskPriority taskPriority = Optional.ofNullable(task.getPriority())
+                .orElse(TaskPriority.MEDIUM);
+
+        TaskStatus taskStatus = TaskStatus.OPEN;
+
+        TaskList taskList=taskListRepository.findById(taskListId)
+            .orElseThrow(() -> new IllegalArgumentException("Task list not found"));
+
+        LocalDateTime now = LocalDateTime.now();
+        Task taskToSave = new Task( 
+            null,
+            task.getTitle(),
+            task.getDescription(),
+            task.getDueDate(),
+            taskStatus,
+            taskPriority,
+            taskList,
+            now,
+            now
+        );  
+        
+        return taskRepository.save(taskToSave);
     }
 
 }
